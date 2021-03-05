678cdabd08d9e97cad16f7d117fc0355f39972a8
Andrey <Andrey.nsk.Pro@gmail.com>
2021-03-06 01:16:00 +0400

    Fix MainArray.java. implemented methods class ArrayStorage.java

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,61 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
+    int count;
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        Arrays.fill(storage, 0, count, null);
+        count = 0;
     }
 
     void save(Resume r) {
+        if (size() <= storage.length) {
+            storage[count] = r;
+            count++;
+        } else {
+            System.out.println("Не хватает места для ввода нового резюме.");
+        }
     }
 
     Resume get(String uuid) {
+        try {
+            for (Resume resume : storage) {
+                if (uuid.equals((resume.uuid))) {
+                    return resume;
+                }
+            }
+        } catch (NullPointerException e) {
+            return null;
+        }
         return null;
     }
 
     void delete(String uuid) {
+        try {
+            for (int i = 0; i <= size(); i++) {
+                if (uuid.equals(storage[i].uuid)) {
+                    System.arraycopy(storage, i + 1, storage, i, size() - i);
+                    count--;
+                    break;
+                }
+            }
+        } catch (NullPointerException e) {
+            System.out.println("Указанное резюме отсутсвует.");
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOf(storage, count);
     }
 
     int size() {
-        return 0;
+        return Arrays.copyOf(storage, count).length;
     }
 }
